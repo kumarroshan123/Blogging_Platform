6244b79b7cde366357449684f3e74beb5845dded
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-07 17:39:57 +0530

    Created_Post_Model

--- blog_platform/Blog/models.py
+++ blog_platform/Blog/models.py
@@ -9,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user}-{self.user_type}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.title}-{self.author}'   
