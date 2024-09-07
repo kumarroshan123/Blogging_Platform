0d12ca8b3e536d401e2bce64ae9beae66c492f60
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-07 17:53:38 +0530

    Comment_Serializer

--- blog_platform/Blog/serializers.py
+++ blog_platform/Blog/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Profile, Post
+from .models import Profile, Post , Comment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +20,9 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = '__all__'
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        
