45f5608203524471f1e61afb9622bd19defd02c3
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-07 17:38:36 +0530

    Created_Profile_Model

--- blog_platform/Blog/models.py
+++ blog_platform/Blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user_roles = (('reader', 'Reader'), ('author', 'Author'), ('admin', 'Admin'))
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=user_roles, default='reader')
+
+    def __str__(self) -> str:
+        return f'{self.user}-{self.user_type}'
