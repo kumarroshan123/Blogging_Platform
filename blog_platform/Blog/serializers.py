6919b6a5d063480a4bf688e0a4d02e982891d450
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-08 14:02:03 +0530

    Implementeda_hashing_Password

--- blog_platform/Blog/serializers.py
+++ blog_platform/Blog/serializers.py
@@ -8,6 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email", "password"]
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self,validated_data):
+        password=validated_data.pop('password',None)
+        instance= User(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,4 +33,3 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        
